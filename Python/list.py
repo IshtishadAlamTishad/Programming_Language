f7e7480c411533fa25a65598bd9687c867e045f3
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-05-29 11:29:36 +0600

    Create list.py

---
+++ Python/list.py
@@ -0,0 +1,11 @@
+friends = ["K","L","X"]
+print(friends)
+
+friends[1] = "MIKE"
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+print(friends[1:])
+print(friends[1:2])
