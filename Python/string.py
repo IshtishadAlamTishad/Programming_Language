b037f394d3b5794298361ce7699de9f703cd21dd
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-05-29 11:30:00 +0600

    Create string.py

---
+++ Python/string.py
@@ -0,0 +1,21 @@
+print("Hello \n user")
+print("Giraffe\"Academy")
+
+phrase = "Giraffe academy"
+print(phrase)
+
+
+print(phrase + " is cool")
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+
+print(len(phrase))
+print("First chracter of "+ phrase + " is = "+phrase[0])
+print(phrase.index("G"))
+
+print(phrase.index("acad"))
+
+print(phrase.replace("Giraffe","Elephant"))
