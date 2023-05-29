4814193d2b421982be6999af6ca8dd545a758bc1
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-05-29 11:30:27 +0600

    Create functions.py

---
+++ Python/functions.py
@@ -0,0 +1,30 @@
+
+def say_hi():
+    print("Hello user")
+
+print("Top")
+say_hi()
+print("Bottom")
+
+def add(x,y): 
+    print(x+y)
+
+add(2,3)
+
+def sub(x,y):
+    print(x-y)
+
+sub(5,2)
+
+def greet(name,age):
+    print("Hello "+name+" your age "+ age)
+
+greet("Tishad","23")
+
+
+def func():
+    name = input("Enter your name : ")
+    age = input("Enter your age : ")
+    print("Hello" + name + ",your age is : " + age)
+
+func()    
