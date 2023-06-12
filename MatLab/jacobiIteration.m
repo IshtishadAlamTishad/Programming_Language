6ca3b55a4124fb8450aa73036501ffcbd8246718
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2023-06-12 21:16:41 +0600

    Update jacobiIteration.m

--- MatLab/jacobiIteration.m
+++ MatLab/jacobiIteration.m
@@ -3,9 +3,13 @@ x(1) = 1.5;
 y(1) = -1;
 z(1) = 1;
 iter(1) = 0;
+
 For i = 1:4
-iter(n+1) = n
+iter(n+1) = n;
+
 x(n+1) = (16+3*y(n)-2*z(n))/8;
 y(n+1) = (9-2*x(n+1)+z(n))/8;
 z(n+1) = (-1-2*x(n+1)+4*y(n+1))/-10;
- solution = (iter;x;y;z)
+end
+
+solution = (iter',x',y',z')
